d717e0aeff55f4dd571f34365a6e358f35f330b1
DoxT1me <arklogolor@gmail.com>
2026-02-14 02:52:56 +0200

    Track total connected users and enhance socket events
    Added total connected users tracking and improved event handling.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Раздаем статические файлы из корня
 app.use(express.static(__dirname));
 
+let totalConnected = 0;
+
 io.on('connection', (socket) => {
+    totalConnected++;
+    io.emit('totalOnline', totalConnected);
+
     socket.on('joinRoom', (roomName) => {
         socket.join(roomName);
         socket.currentRoom = roomName;
@@ -18,22 +21,26 @@ io.on('connection', (socket) => {
         const room = io.sockets.adapter.rooms.get(roomName);
         const numClients = room ? room.size : 0;
 
-        // Игрок 1 получает сторону 1, Игрок 2 сторону 2
+        // Передаем роль и общее кол-во игроков в комнате
         socket.emit('playerRole', numClients);
         io.to(roomName).emit('playerCount', numClients);
     });
 
     socket.on('spawnUnit', (data) => {
-        // Пересылаем данные только игрокам в той же комнате
-        socket.to(socket.currentRoom).emit('spawnUnit', data);
+        if (socket.currentRoom) {
+            socket.to(socket.currentRoom).emit('spawnUnit', data);
+        }
     });
 
     socket.on('startGame', () => {
-        io.to(socket.currentRoom).emit('gameStart');
+        if (socket.currentRoom) {
+            io.to(socket.currentRoom).emit('gameStart');
+        }
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        totalConnected--;
+        io.emit('totalOnline', totalConnected);
     });
 });
 
