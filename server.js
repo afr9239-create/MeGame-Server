2d040f577c2fa8e7da24f1a4876cd3a4507278e4
Vitalii Ruzhytski <afr9239@gmail.com>
2026-02-14 18:42:28 +0200

    Refactor server.js for room management and events

--- server.js
+++ server.js
@@ -6,79 +6,37 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(express.static(__dirname));
-
-let totalConnected = 0;
-// Храним данные комнат: ХП баз и состояние игры
-const roomsData = {};
+const rooms = {}; // Хранилище данных комнат
 
 io.on('connection', (socket) => {
-    totalConnected++;
-    io.emit('totalOnline', totalConnected);
+    console.log('Новое подключение:', socket.id);
 
-    socket.on('joinRoom', (roomName) => {
-        socket.join(roomName);
-        socket.currentRoom = roomName;
+    socket.on('joinLobby', ({ roomId, nickname }) => {
+        socket.join(roomId);
+        
+        if (!rooms[roomId]) rooms[roomId] = [];
+        
+        // Добавляем игрока в список комнаты
+        rooms[roomId].push({ id: socket.id, nickname: nickname });
 
-        if (!roomsData[roomName]) {
-            roomsData[roomName] = {
-                hp1: 1000,
-                hp2: 1000,
-                gameActive: false
-            };
-        }
-
-        const room = io.sockets.adapter.rooms.get(roomName);
-        const numClients = room ? room.size : 0;
-
-        socket.emit('playerRole', numClients);
-        io.to(roomName).emit('playerCount', numClients);
+        // Рассылаем всем в комнате обновленный список
+        io.to(roomId).emit('updatePlayerList', rooms[roomId]);
     });
 
-    // Когда юнит бьет базу, клиент шлет 'baseDamage'
-    socket.on('baseDamage', (data) => {
-        const room = roomsData[socket.currentRoom];
-        if (room && room.gameActive) {
-            // Уменьшаем ХП на сервере
-            if (data.targetSide === 1) room.hp1 -= data.damage;
-            if (data.targetSide === 2) room.hp2 -= data.damage;
-
-            // Рассылаем актуальное ХП всем
-            io.to(socket.currentRoom).emit('updateHP', {
-                hp1: room.hp1,
-                hp2: room.hp2
-            });
-
-            // Проверка победы
-            if (room.hp1 <= 0 || room.hp2 <= 0) {
-                const winner = room.hp1 <= 0 ? 2 : 1;
-                io.to(socket.currentRoom).emit('gameOver', { winner });
-                room.gameActive = false;
-                // Сброс ХП для нового раунда
-                room.hp1 = 1000; room.hp2 = 1000;
-            }
-        }
-    });
-
-    socket.on('spawnUnit', (data) => {
-        if (socket.currentRoom) {
-            socket.to(socket.currentRoom).emit('spawnUnit', data);
-        }
-    });
-
-    socket.on('startGame', () => {
-        if (socket.currentRoom && roomsData[socket.currentRoom]) {
-            roomsData[socket.currentRoom].gameActive = true;
-            io.to(socket.currentRoom).emit('gameStart');
-        }
+    socket.on('startGameRequest', (roomId) => {
+        io.to(roomId).emit('gameStarted');
     });
 
     socket.on('disconnect', () => {
-        totalConnected--;
-        io.emit('totalOnline', totalConnected);
-        // Можно добавить удаление данных комнаты, если она пуста
+        // Удаляем игрока из всех комнат при выходе
+        for (const roomId in rooms) {
+            rooms[roomId] = rooms[roomId].filter(p => p.id !== socket.id);
+            io.to(roomId).emit('updatePlayerList', rooms[roomId]);
+            if (rooms[roomId].length === 0) delete rooms[roomId];
+        }
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Сервер War Castle 2.0 запущен!');
+});
