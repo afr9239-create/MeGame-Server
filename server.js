d7b878b6cf52b47580e9a7d34aa45e80d5b7f1e6
afr9239-create <afr9239@gmail.com>
2026-02-14 14:12:44 +0200

    Implement room data and game state management
    Added room data management for game state and player HP.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(__dirname));
 
 let totalConnected = 0;
+// Храним данные комнат: ХП баз и состояние игры
+const roomsData = {};
 
 io.on('connection', (socket) => {
     totalConnected++;
@@ -18,12 +20,44 @@ io.on('connection', (socket) => {
         socket.join(roomName);
         socket.currentRoom = roomName;
 
+        if (!roomsData[roomName]) {
+            roomsData[roomName] = {
+                hp1: 1000,
+                hp2: 1000,
+                gameActive: false
+            };
+        }
+
         const room = io.sockets.adapter.rooms.get(roomName);
         const numClients = room ? room.size : 0;
 
-        // Передаем роль и общее кол-во игроков в комнате
         socket.emit('playerRole', numClients);
         io.to(roomName).emit('playerCount', numClients);
+    });
+
+    // Когда юнит бьет базу, клиент шлет 'baseDamage'
+    socket.on('baseDamage', (data) => {
+        const room = roomsData[socket.currentRoom];
+        if (room && room.gameActive) {
+            // Уменьшаем ХП на сервере
+            if (data.targetSide === 1) room.hp1 -= data.damage;
+            if (data.targetSide === 2) room.hp2 -= data.damage;
+
+            // Рассылаем актуальное ХП всем
+            io.to(socket.currentRoom).emit('updateHP', {
+                hp1: room.hp1,
+                hp2: room.hp2
+            });
+
+            // Проверка победы
+            if (room.hp1 <= 0 || room.hp2 <= 0) {
+                const winner = room.hp1 <= 0 ? 2 : 1;
+                io.to(socket.currentRoom).emit('gameOver', { winner });
+                room.gameActive = false;
+                // Сброс ХП для нового раунда
+                room.hp1 = 1000; room.hp2 = 1000;
+            }
+        }
     });
 
     socket.on('spawnUnit', (data) => {
@@ -33,7 +67,8 @@ io.on('connection', (socket) => {
     });
 
     socket.on('startGame', () => {
-        if (socket.currentRoom) {
+        if (socket.currentRoom && roomsData[socket.currentRoom]) {
+            roomsData[socket.currentRoom].gameActive = true;
             io.to(socket.currentRoom).emit('gameStart');
         }
     });
@@ -41,6 +76,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         totalConnected--;
         io.emit('totalOnline', totalConnected);
+        // Можно добавить удаление данных комнаты, если она пуста
     });
 });
 
